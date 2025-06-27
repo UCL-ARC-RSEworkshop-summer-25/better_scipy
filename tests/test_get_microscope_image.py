560c1f809cc72e6932de5981b636260b874d6c65
Hannah CP <hannahcp@outlook.com>
2025-06-27 16:40:34 +0100

    Add working test file for get image

--- tests/test_get_microscope_image.py
+++ tests/test_get_microscope_image.py
@@ -1,8 +1,10 @@
 import numpy as np
-from microscope.get_microscope_image import get_image
+from better_scipy.get_microscope_image import get_image
+import pytest
 
+@pytest.mark.online
 def test_get_image_check_image_size():
-    test = get_image.get_image()
+    test = get_image()
 
     result = np.shape(test)
     expected = (480, 640, 3)
